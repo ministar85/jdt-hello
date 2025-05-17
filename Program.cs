05eef0c0676ae56f47d96a8ca89e427400ab6839
ministar85 <72354295+ministar85@users.noreply.github.com>
2025-05-18 01:51:53 +0200

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello from JDT Hello World!");
-
-app.MapGet("/health", () => Results.Ok("Healthy"));
-app.MapGet("/ready", () => Results.Ok("Ready"));
-
-app.Run();
